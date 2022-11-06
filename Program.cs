0f6e7bcf991f05659bf21488dc3cbea2357d9b72
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-11-06 15:53:12 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexV
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the regex and lambda program");
+        }
+    }
+}
+      
+       
